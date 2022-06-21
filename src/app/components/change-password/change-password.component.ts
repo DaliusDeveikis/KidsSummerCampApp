8b378fd811e53c67583be56f78c5ec36b02e9cfd
DaliusDeveikis <slkyper@gmail.com>
2022-06-21 20:21:02 +0300

    change password errors information

--- src/app/components/change-password/change-password.component.ts
+++ src/app/components/change-password/change-password.component.ts
@@ -11,6 +11,7 @@ import { AuthService } from 'src/app/services/auth.service';
 })
 export class ChangePasswordComponent implements OnInit {
   public user: User | null = null
+  public error: string = ''
 
 
   constructor(private auth:AuthService, private router:Router) { }
@@ -22,8 +23,17 @@ export class ChangePasswordComponent implements OnInit {
     this.router.navigate(["/"]);
   };
 
-  private errorFunc=()=>{
-    return {"error": "An error"}
+  private errorFunc=(response:any)=>{
+    console.log(response);
+    switch(response.error.error.message){
+      case "INVALID_ID_TOKEN":
+              this.error="Toks email egzistuoja";
+              break;
+  
+      case "WEAK_PASSWORD : Password should be at least 6 characters":
+            this.error="Slaptažodis turi būti ne trumpesnis nei 6 simboliai";
+            break;       
+    }
   };
 
   public changePassword(form:NgForm) {
